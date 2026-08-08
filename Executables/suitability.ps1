23a741f3966ad1311dc30c217e85c5051c2b8834
Kwanteks <khanwbizz@gmail.com>
2026-08-08 09:01:20 -0400

    UAC revert

--- Executables/suitability.ps1
+++ Executables/suitability.ps1
@@ -1,7 +1,6 @@
 Add-Type -AssemblyName System.Windows.Forms -ErrorAction SilentlyContinue
 
 $title      = 'Unsupported Installation'
-$msAccMsg   = 'Microsoft user accounts are not supported. Please try again on a local user account.'
 $oemMsg     = 'Custom OS/OEM installations are not supported. Please try again on a fresh installation of Windows.'
 if (Test-Path -LiteralPath 'C:\SOS') {
     $global:LASTEXITCODE = 0
@@ -19,9 +18,6 @@ function Show-Error {
     exit 1
 }
 
-if ((Get-LocalUser $env:USERNAME).PrincipalSource -ne 'Local') {
-    Show-Error $msAccMsg
-}
 
 $oem = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation' -ErrorAction SilentlyContinue
 if ($oem) {
